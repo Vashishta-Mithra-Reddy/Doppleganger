15d0b0b17d9d328213febae35a4c5b1a47cefdd4
Vashishta Mithra Reddy Vundyala <vashishtamithrareddy@gmail.com>
2025-02-02 19:30:23 +0530

    fix2

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
           <main className="min-h-screen flex flex-col items-center">
             <div className="flex-1 w-full flex flex-col gap-20 items-center">
               <nav className="w-full flex justify-center items-center border-b border-b-foreground/10 h-16 py-10">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+                <div className="w-full max-w-6xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold text-xl">
                     <Link href={"/"}>Doppelgänger</Link>
                   </div>
