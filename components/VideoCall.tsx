6095fa968e1461c2a8de5b45c79c38c75a2f9003
Vashishta Mithra Reddy Vundyala <vashishtamithrareddy@gmail.com>
2025-02-05 12:21:44 +0530

    Fix 14

--- components/VideoCall.tsx
+++ components/VideoCall.tsx
@@ -324,6 +324,7 @@ const VideoCall: React.FC<VideoCallProps> = ({ roomId, userId }) => {
                 muted
                 playsInline
                 className="w-full h-64 object-cover rounded-lg bg-gray-900"
+                style={{ transform: "scaleX(-1)" }}
               />
               <div className="absolute bottom-2 left-2 bg-black bg-opacity-50 text-white px-2 py-1 rounded">
                 You
@@ -335,6 +336,7 @@ const VideoCall: React.FC<VideoCallProps> = ({ roomId, userId }) => {
                 autoPlay
                 playsInline
                 className="w-full h-64 object-cover rounded-lg bg-gray-900"
+                style={{ transform: "scaleX(-1)" }}
               />
               <div className="absolute bottom-2 left-2 bg-black bg-opacity-50 text-white px-2 py-1 rounded">
                 Remote User
